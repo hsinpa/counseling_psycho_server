a5441f1ba8eeba07e7774a8b8db1d2a29bd4f0e2
Hsinpa <hsinpa@gmail.com>
2024-06-27 09:33:14 +0800

    Add swagger

--- src/app.py
+++ src/app.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 
-from src.utility.utility_method import chunk
-
-app = FastAPI()
-print(list(chunk([1, 2, 3, 4, 5, 6], 2)))
-
+app = FastAPI(openapi_url="/docs/openapi.json", docs_url="/docs")
 
 @app.get("/")
 async def root():
-    return {"Say": "Hello World!"}
+    return {"version": "0.0.2"}
