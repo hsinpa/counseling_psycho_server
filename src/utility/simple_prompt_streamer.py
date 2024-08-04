7996bb9d908823969ecfb127dee66df188016e34
Hsinpa <hsinpa@gmail.com>
2024-08-04 20:10:38 +0800

    hotfix => websocket  get function

--- src/utility/simple_prompt_streamer.py
+++ src/utility/simple_prompt_streamer.py
@@ -4,13 +4,14 @@ from typing import Any
 from langchain_core.runnables import RunnableSerializable
 
 from src.model.general_model import StreamingDataChunkType, DataChunkType, SocketEvent
-from src.websocket.websocket_manager import websocket_manager
+from src.websocket.websocket_manager import get_websocket
 
 
 class SimplePromptStreamer:
     def __init__(self, user_id: str, session_id: str):
         self._user_id = user_id
         self._session_id = session_id
+        self._websocket_manager = get_websocket()
 
     async def execute(self, chain: RunnableSerializable[dict, Any], p_input: dict = {}):
         results = ''
@@ -21,12 +22,12 @@ class SimplePromptStreamer:
             stream_data = StreamingDataChunkType(session_id=self._session_id, data=data_chunk, type=DataChunkType.Chunk)
             json_string = {'event': SocketEvent.bot, **stream_data.model_dump()}
 
-            await websocket_manager.send(target_id=self._user_id, data=json.dumps(json_string, ensure_ascii=False))
+            await self._websocket_manager.send(target_id=self._user_id, data=json.dumps(json_string, ensure_ascii=False))
             results = results + data_chunk
 
         stream_data = StreamingDataChunkType(session_id=self._session_id, data=results, type=DataChunkType.Complete)
         json_string = {'event': SocketEvent.bot, **stream_data.model_dump()}
 
-        await websocket_manager.send(target_id=self._user_id, data=json.dumps(json_string, ensure_ascii=False))
+        await self._websocket_manager.send(target_id=self._user_id, data=json.dumps(json_string, ensure_ascii=False))
 
         return results
