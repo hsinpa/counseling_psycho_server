13168864ed8a6ff513ae9038697b2d16826b66bb
Hsinpa <hsinpa@gmail.com>
2024-09-06 15:32:54 +0800

    Updating prompt

--- src/llm_agents/prompt/chatbot_kg_distill_prompt.py
+++ src/llm_agents/prompt/chatbot_kg_distill_prompt.py
@@ -1,10 +1,23 @@
 KG_DISTILL_SYSTEM = """\
-please think step by step.  
-給予一段"使用者的輸入"，step1:請幫我擷取整段話裡最重要的訊息。
-step2: 請根據"使用者的輸入"與step1擷取的訊息，將"使用者的輸入"做成一個使用者Knowledge Graph，
-用sub graph標示主題，並以triple的方式呈現，並將最重要的triple挑出來做為關鍵triple。
+我打算繪製一幅Knowledge graph, 而你是世上最好的關係分析師
 
+請依照以下順序做事
+1. 根據 '使用者回覆', 仔細思考 並分析整段話中 重要的訊息為何。
+2. 將[Past History]作為輔助用的參考資料, 把 '使用者回覆' 和 '思考過程',  以Json 的方式呈現 使用者Knowledge Graph，
 
+The format of knowledge map is layout in JSON format, and you need to strictly follow the rule.
+Please order the JSON array by important level, sort the most important object on top.
+
+{
+    "thought_process": "分析整段話中 重要的訊息為何",
+    "knowledge_graph": [
+        {
+            "node_1": "Label of node",
+            "relationship": "The name of directional or unidirectional relationship",
+            "node_2": "Label of connected node"
+        }
+    ]
+}
 """
 
 KG_DISTILL_QUERY = """
