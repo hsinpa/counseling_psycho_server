6ca97e9836f855176ba663f537857299e9e18d24
Hsinpa <hsinpa@gmail.com>
2024-10-18 15:27:11 +0800

    gemini to 002

--- src/utility/static_text.py
+++ src/utility/static_text.py
@@ -1,5 +1,5 @@
 OpenAI_Model_4o = 'gpt-4o'
 OpenAI_Model_3_5 = 'gpt-3.5-turbo'
 OpenAI_Model_4o_mini = 'gpt-4o-mini'
-Gemini_Model_1_5 = 'gemini-1.5-flash-001'
+Gemini_Model_1_5 = 'gemini-1.5-flash-002'
 
